d7c791470fee69c2c811cb7e575d4d394348c87c
vankovap <petravankova.pv@gmail.com>
2024-01-30 22:56:00 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,13 +2,13 @@ import http.server
 import socketserver
 from http import HTTPStatus
 
+PORT=8000
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        self.wfile.write(b'Hello world')
+        self.wfile.write(b'Hello World')
 
-
-httpd = socketserver.TCPServer(('', 8000), Handler)
-httpd.serve_forever()
+with socketserver.TCPServer(('', PORT), Handler) as httpd:
+    httpd.serve_forever()
