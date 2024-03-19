1959228dbee949c8530b5838f2ab5e871e446fa5
vankovap <petravankova.pv@gmail.com>
2024-03-19 23:19:20 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,15 @@
 import http.server
 import socketserver
 from http import HTTPStatus
+import logging
+logger = logging.getLogger(__name__)
 
 PORT=8000
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        logger.info('Started')
+        logger.warning('Finished')
         self.send_response(HTTPStatus.OK)
         self.end_headers()
         self.wfile.write(b'Hello World')
