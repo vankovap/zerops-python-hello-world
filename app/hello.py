0f953f9b2154c2f67ab15c20636cac0547403af2
vankovap <petravankova.pv@gmail.com>
2024-04-09 23:57:40 +0200

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -1,7 +1,5 @@
 from flask import Flask
 
-PORT=8000
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,4 +8,4 @@ def hello():
    return "Hello World!"
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0", PORT)
+   app.run(host="0.0.0.0", port=8000)
